4ddd6ce1ce6c47a554b6aae96fff6a8463357488
Abraham Egnor <abraham.egnor@mongodb.com>
2022-08-19 10:57:42 -0400

    run on serverless

--- .evergreen/run-serverless-tests.sh
+++ .evergreen/run-serverless-tests.sh
@@ -47,7 +47,8 @@ cargo_test test::spec::load_balancers > load_balancers.xml
 cargo_test test::cursor > cursor.xml
 cargo_test test::spec::collection_management > coll.xml
 cargo_test test::spec::command_monitoring_unified > monitoring.xml
+cargo test test::spec::change_streams > change_streams.xml
 
-junit-report-merger results.xml crud.xml retryable_reads.xml retryable_writes.xml versioned_api.xml sessions.xml transactions.xml load_balancers.xml cursor.xml coll.xml monitoring.xml
+junit-report-merger results.xml crud.xml retryable_reads.xml retryable_writes.xml versioned_api.xml sessions.xml transactions.xml load_balancers.xml cursor.xml coll.xml monitoring.xml change_streams.xml
 
 exit $CARGO_RESULT
